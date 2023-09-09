70a56af23d194a9127110f1566ad0b09b7396be3
Kamran <123491415+KAMRANANSARI01@users.noreply.github.com>
2023-09-09 19:23:41 +0530

    added footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
-import"./style.scss"
-const Footer = () => {
-  return (
-    <div>
-      footer
-    </div>
-  )
-}
+import React from "react";
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+} from "react-icons/fa";
 
-export default Footer
+import ContentWrapper from '../../components/contenWrapper/ContentWrapper'
+
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                <ul className="menuItems">
+                    <li className="menuItem">Terms Of Use</li>
+                    <li className="menuItem">Privacy-Policy</li>
+                    <li className="menuItem">About</li>
+                    <li className="menuItem">Blog</li>
+                    <li className="menuItem">FAQ</li>
+                </ul>
+                <div className="infoText">
+                Say goodbye to buffering and interruptions. Flixify offers lightning-fast, high-quality streaming, ensuring you enjoy your favorite content in crystal-clear resolution. Our user-friendly interface lets you navigate effortlessly, making it a breeze to find what you want to watch.
+                </div>
+                <div className="socialIcons">
+                    <span className="icon">
+                        <FaFacebookF />
+                    </span>
+                    <span className="icon">
+                        <FaInstagram />
+                    </span>
+                    <span className="icon">
+                        <FaTwitter />
+                    </span>
+                    <span className="icon">
+                        <FaLinkedin />
+                    </span>
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,7 +5,6 @@ const Home = () => {
   return (
     <div>
       <HeroBanner/>
-      <div style={{height:1000}}></div>
     </div>
   )
 }
