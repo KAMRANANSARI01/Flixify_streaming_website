d175856198ec65b6ee619b3b935b5d08e50e73a9
Kamran <123491415+KAMRANANSARI01@users.noreply.github.com>
2023-09-16 23:06:23 +0530

    home section errror resolved

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import "./style.scss"
-import HeroBanner from './heroBanner/HeroBanner'
-import Trending from './trending/Trending'
-import ContentWrapper from '../../components/contenWrapper/ContentWrapper'
-import Popular from './popular/Popular'
-import TopRated from './topRated/TopRated'
+import React from "react";
+import "./style.scss";
+import HeroBanner from "./heroBanner/HeroBanner";
+import Trending from "./trending/Trending";
+import ContentWrapper from "../../components/contenWrapper/ContentWrapper";
+import Popular from "./popular/Popular";
+import TopRated from "./topRated/TopRated";
 
 const Home = () => {
   return (
     <div>
-      <HeroBanner/>
-      <Trending/>
-      <Popular/>
-      <TopRated/>
+      <HeroBanner />
+      <Trending />
+      <Popular />
+      <TopRated />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
