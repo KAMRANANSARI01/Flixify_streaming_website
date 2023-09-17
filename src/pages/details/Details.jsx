3aa03284d11990515756e4ad4353fc041f453dbd
Kamran <123491415+KAMRANANSARI01@users.noreply.github.com>
2023-09-18 00:42:22 +0530

    details banner and top cast added

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
+import useFetch from '../../hooks/useFetch';
+import DetailsBanner from './detailsBanner/DetailsBanner'
+import Cast from './cast/Cast';
 import "./style.scss"
+// import { useSelector } from 'react-redux';
 const Details = () => {
+  const {mediaType, id} = useParams();
+  const {data,loading} =useFetch(`/${mediaType}/${id}/videos`);
+  const {data:credits,loading:creditLoading} = useFetch(`/${mediaType}/${id}/credits`);
+  // const {url} = useSelector((state)=>state.home)
+
+  
   return (
     <div>
-      details
+      <DetailsBanner video={data?.results?.[0]} crew = {credits?.crew}/>
+      <Cast data={credits?.cast} loading={creditLoading}/>
+
     </div>
   )
 }
